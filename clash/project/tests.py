809c47325a62fc6c90bb275b29ff30963fd143b5
suyash-more <suyashmore789@gmail.com>
2021-08-26 16:23:02 +0530

    tests added

--- clash/project/tests.py
+++ clash/project/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Questions
 
-# Create your tests here.
+class QuestionTestCase(TestCase):
+    def testQuestion(self):
+        question = Questions(question="Which laptop is good?", option_A="dell", option_B="hp", option_C="lenovo", option_D="acer", 
+        correct_answer="hp")
+        self.assertEqual(question.question, "Which laptop is good?")
+        self.assertEqual(question.option_A, "dell")
+        self.assertEqual(question.option_B, "hp")
+        self.assertEqual(question.option_C, "lenovo")
+        self.assertEqual(question.option_D, "acer")
+        self.assertEqual(question.correct_answer, "hp")
+
